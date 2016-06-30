ce365d0804f128319275ad4c0b4904c0f768ba91
mazur-nadiia <mazur.nadiia@gmail.com>
2016-07-01 00:26:14 +0300

    Useless variables removed

--- test_create_issues.rb
+++ test_create_issues.rb
@@ -14,7 +14,7 @@ class CreateIssue <Test::Unit::TestCase
 
   def test_create_issue_bug
     user_login('nma', '1234567')
-    issue_name = create_project
+    create_project
     create_issue("Bug", "test issue"+ rand(999).to_s, "test descirption" + rand(999).to_s)
     verify_flash_notice_partial_text(['Issue', 'created.'])
     #verify_issue_created(issue_name)
@@ -22,7 +22,7 @@ class CreateIssue <Test::Unit::TestCase
 
   def test_create_issue_feature
     user_login('nma', '1234567')
-    issue_name = create_project
+    create_project
     create_issue("Feature", "test issue"+ rand(999).to_s, "test descirption" + rand(999).to_s)
     verify_flash_notice_partial_text(['Issue', 'created.'])
     #verify_issue_created(issue_name)
@@ -30,7 +30,7 @@ class CreateIssue <Test::Unit::TestCase
 
   def test_create_issue_support
     user_login('nma', '1234567')
-    issue_name = create_project
+    create_project
     create_issue("Support", "test issue"+ rand(999).to_s, "test descirption" + rand(999).to_s)
     verify_flash_notice_partial_text(['Issue', 'created.'])
     #verify_issue_created(issue_name)
